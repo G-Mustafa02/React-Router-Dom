e811f7ec411614c2a11a0e5ccfd43bfb26ca9c18
Ghulam E Mustafa <uzairmustafa963@gmail.com>
2025-04-05 23:20:32 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './screens/Home'
+import About from './screens/About'
+import Contact from './screens/Contact'
+import Services from './screens/Services'
+import Layout from './Layout'
+import Product from './screens/Product'
+
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Layout/>,
+  children: [
+    {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "about",
+    element: <About/>
+  },
+  {
+    path: "contact",
+    element: <Contact/>
+  },
+  {
+    path: "services",
+    element: <Services/>
+  },
+  {
+    path: 'product/:id',
+    element: <Product/>
+  },
+  {
+    path:'*',
+    element: <Home/>
+  }
+]
+}])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
